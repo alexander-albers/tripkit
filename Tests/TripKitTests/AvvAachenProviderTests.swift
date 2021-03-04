462897b8fab132f36c5253ac9ab09d1e72563983
alexander-albers <27898390+alexander-albers@users.noreply.github.com>
2021-03-04 15:12:34 +0100

    Debug tests

--- Tests/TripKitTests/AvvAachenProviderTests.swift
+++ Tests/TripKitTests/AvvAachenProviderTests.swift
@@ -8,6 +8,7 @@ class AvvAachenProviderTests: TripKitProviderTestCase, TripKitProviderTestsDeleg
     var networkId: NetworkId { return .AVV2 }
     
     func initProvider(from authorizationData: AuthorizationData) -> NetworkProvider {
+        print("HCIKEYS: \(authorizationData.hciAuthorization.keys)")
         return AvvAachenProvider(apiAuthorization: authorizationData.hciAuthorization)
     }
     
