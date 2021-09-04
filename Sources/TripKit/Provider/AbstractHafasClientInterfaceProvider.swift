d72bc7e4a5c3d49e3eddb183b61da958f724c576
alexander-albers <27898390+alexander-albers@users.noreply.github.com>
2021-09-04 10:15:08 +0200

    Replace print statements with unified logging

--- Sources/TripKit/Provider/AbstractHafasClientInterfaceProvider.swift
+++ Sources/TripKit/Provider/AbstractHafasClientInterfaceProvider.swift
@@ -735,7 +735,7 @@ public class AbstractHafasClientInterfaceProvider: AbstractHafasProvider {
         }
         if error != "OK" {
             let errTxt = svcRes["errTxt"] as? String ?? ""
-            os_log("Hafas error %{public}@: %{public}@", log: .requestLogger, type: .error, error, errorTxt)
+            os_log("Hafas error %{public}@: %{public}@", log: .requestLogger, type: .error, error, errTxt)
             if error == "LOCATION" {
                 completion(httpRequest, .invalidId)
             } else if error == "FAIL" || error == "CGI_READ_FAILED" {
