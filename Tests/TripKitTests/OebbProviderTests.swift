3c1768c3e58e2f92ca4636f753e283b54ec41437
alexander-albers <27898390+alexander-albers@users.noreply.github.com>
2021-09-09 11:39:02 +0200

    ÖBB: remove test

--- Tests/TripKitTests/OebbProviderTests.swift
+++ Tests/TripKitTests/OebbProviderTests.swift
@@ -14,33 +14,4 @@ class OebbProviderTests: TripKitProviderTestCase, TripKitProviderTestsDelegate {
         return OebbProvider(apiAuthorization: authorizationData.hciAuthorization)
     }
     
-    func testLineFullNumber() {
-        let (_, result) = syncQueryTrips(from: Location(id: "5600582"),  // Bratislava-Petrzalka
-                                    via: nil,
-                                    to: Location(id: "8101051"),    // Hbf, Wien
-                                    date: Date(),
-                                    departure: true,
-                                    products: nil,
-                                    optimize: nil,
-                                    walkSpeed: nil,
-                                    accessibility: nil,
-                                    options: nil)
-
-        switch result {
-        case .success(_, _, _, _, let trips, let messages):
-            os_log("success: %@, messages=%@", log: .testsLogger, type: .default, trips, messages)
-            XCTAssert(!trips.isEmpty, "received empty result")
-
-            if let trip = trips.first {
-                if let leg = trip.legs.first as? PublicLeg {
-                    XCTAssertNotNil(leg.line.vehicleNumber, "vehicleNumber must be not be nil")
-                }
-            }
-        case .failure(let error):
-            XCTFail("received an error: \(error)")
-        default:
-            XCTFail("illegal result type \(result)")
-        }
-    }
-    
 }
