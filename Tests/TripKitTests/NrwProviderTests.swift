47fe4277237f136a09a497cd4447af3e079657d9
alexander-albers <27898390+alexander-albers@users.noreply.github.com>
2024-12-30 17:32:48 +0100

    Fix static tests

--- Tests/TripKitTests/NrwProviderTests.swift
+++ Tests/TripKitTests/NrwProviderTests.swift
@@ -6,6 +6,8 @@ class NrwProviderTests: TripKitProviderTestCase, TripKitProviderTestsDelegate {
     
     override var delegate: TripKitProviderTestsDelegate! { return self }
     
+    var supportsQueryMoreTrips: Bool { return false }
+    
     var networkId: NetworkId { return .NRW }
     
     func initProvider(from authorizationData: AuthorizationData) -> NetworkProvider {
